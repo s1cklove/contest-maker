8c0912b3bbcee23ea1e4ef534ed0bc464926cb72
s1cklove <roman.s.zinin@gmail.com>
2024-09-11 02:13:55 +0300

    Add access file

---
+++ src/get_access.py
@@ -0,0 +1,16 @@
+import json
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+
+with open('../assets/scopes.json') as f:
+    scopes = json.load(f)['links']
+
+credentials = service_account.Credentials.from_service_account_file(
+    "../oauth.json", scopes=scopes)
+
+services = {
+    "forms": build('forms', 'v1', credentials=credentials),
+    "drive": build('drive', 'v3', credentials=credentials),
+    "sheets": build('sheets', 'v4', credentials=credentials),
+    # "script": build('script', 'v1', credentials=credentials)
+}
